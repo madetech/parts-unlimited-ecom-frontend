10979cd86e3c7e0fce8e9dab4e3d9b067fc682fb
Jaseera Abubacker <j.p.mohamedabubacker@gmail.com>
2018-08-23 11:54:49 +0100

    wrap everything in a describe block and mount in a beforeEach block

--- src/components/CustomerDetails/CustomerDetails.test.js
+++ src/components/CustomerDetails/CustomerDetails.test.js
@@ -2,14 +2,19 @@ import React from 'react';
 import {mount} from 'enzyme';
 import CustomerDetails from '../CustomerDetails';
 
-const wrapper = mount(
-  <CustomerDetails/>
-);
+describe('CustomerDetails', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = mount(
+        <CustomerDetails/>
+      );
+  });
 
-it('render a customer form Shipping Details heading ', () => {
-  expect(wrapper.find("h3").at(0).text()).toEqual("Shipping Details");
-});
+  it('render a customer form first heading ', () => {
+    expect(wrapper.find("h3").at(0).text()).toEqual("Shipping Details");
+  });
 
-it('render a customer form label City ', () => {
-  expect(wrapper.find("FormItem").at(3).text()).toEqual("City");
-});
+  it('render a customer form fourth label ', () => {
+    expect(wrapper.find("FormItem").at(3).text()).toEqual("City");
+  });
+})
