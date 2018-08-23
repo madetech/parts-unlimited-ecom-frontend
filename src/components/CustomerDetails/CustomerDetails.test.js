cce031d4382f34e97614383a5d8b2ef84e7cf4a3
Jaseera Abubacker <j.p.mohamedabubacker@gmail.com>
2018-08-23 15:19:17 +0100

    add new test and refactor test

--- src/components/CustomerDetails/CustomerDetails.test.js
+++ src/components/CustomerDetails/CustomerDetails.test.js
@@ -10,34 +10,38 @@ describe('CustomerDetails', () => {
       );
   });
 
-  it('render a customer form first heading ', () => {
+  it('renders a customer form first heading ', () => {
     expect(wrapper.find("h3").at(0).text()).toEqual("Shipping Details");
   });
 
-  it('render a customer form fourth label ', () => {
+  it('renders a customer form fourth label ', () => {
     expect(wrapper.find("FormItem").at(3).text()).toEqual("City");
   });
 
-  it('has two postcodes for shipping + billing', () => {
+  it('renders a customer form second step', () => {
+    expect(wrapper.find("Step").at(1).text()).toEqual("Items")
+  })
+
+  it('contains two postcodes for shipping + billing', () => {
     expect(wrapper.find('[label="Postcode"]').length).toEqual(2);
+  })
+
+  it('conatains fifteen form label items', () => {
+    expect(wrapper.find('FormItem[label]').length).toEqual(15);
+  })
+
+  it('contains three steps progress dot', () => {
+    expect(wrapper.find("Step").length).toEqual(3);
   })
 
   it('contains a submit button', () => {
     expect(wrapper.find('[htmlType="submit"]').length).toEqual(1);
   })
 
-  it('set customer name barry', () => {
+  it('sets and returns the customer name barry', () => {
     const customername = wrapper.find('Input').at(0)
     customername.value = 'Barry';
     expect(customername.value).toEqual("Barry");
    })
-
-  it('has three steps', () => {
-    expect(wrapper.find("Step").length).toEqual(3);
-  })
-
-  it('renders a customer form second step', () => {
-    expect(wrapper.find("Step").at(1).text()).toEqual("Items")
-  })
 })
 
