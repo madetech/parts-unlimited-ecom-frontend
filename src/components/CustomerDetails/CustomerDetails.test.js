b19429ad8dbab9494208fcc73f546b757ada00c2
Jaseera Abubacker <j.p.mohamedabubacker@gmail.com>
2018-08-23 11:34:19 +0100

    fixed code from PR comments

--- src/components/CustomerDetails/CustomerDetails.test.js
+++ src/components/CustomerDetails/CustomerDetails.test.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import {mount} from 'enzyme';
 import CustomerForm from '../CustomerForm';
-it('render a form', () => {
-  const wrapper = mount(
-      <div><div className="foo">Hello</div></div>
-  );
-  expect(wrapper.find(".foo").text()).toEqual("Hello");
-});
+
+const wrapper = mount(
+  <CustomerForm/>
+);
 
 it('render a customer form Shipping Details heading ', () => {
-  const wrapper = mount(
-    <CustomerForm/>
-  );
   expect(wrapper.find("h3").at(0).text()).toEqual("Shipping Details");
 });
 
 it('render a customer form label City ', () => {
-  const wrapper = mount(
-    <CustomerForm/>
-  );
   expect(wrapper.find("FormItem").at(3).text()).toEqual("City");
 });
