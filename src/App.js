813ef9f0899fafc37fa92d8c55a56bfe695e3cfe
George Schena <george@madetech.com>
2018-08-28 10:16:49 +0100

    work on steps changing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import CustomerDetails from './components/CustomerDetails';
+import Theme from './components/Theme';
 
 class App extends Component {
   render() {
-    return <CustomerDetails/>
+    return <Theme><CustomerDetails/></Theme>
   }
 }
 
