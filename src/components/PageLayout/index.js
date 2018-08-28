eeb1bc8067dc7840773fed383c4d4c2476d60be4
rebecca fitzsimmons <rebecca@madetech.com>
2018-08-28 10:27:16 +0100

    import css from emotion

--- src/components/PageLayout/index.js
+++ src/components/PageLayout/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import CustomerDetails from '../CustomerDetails';
+import { css } from 'emotion';
 
 export default function PageLayout(props){
   return <CustomerDetails />
