64ec1ad3557c662bf08a3a3d0cc7c7e0a38bb338
Craig J. Bass <craig@madetech.co.uk>
2018-08-30 12:03:06 +0100

    Debugging around equality of our objects

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 import { mount } from 'enzyme';
 import { promises as fsPromises } from 'fs';
@@ -11,7 +10,7 @@ describe('<MyComponent /', () => {
       customerName: "Harry Harlow",
     }
     wrapper.find('input[data-test="customer-name"]').simulate('change', {target: {value: 'Harry Harlow'}})
-    await wrapper.find('button').simulate('submit')
+    wrapper.find('button').simulate('submit')
     let fileNames = await fsPromises.readdir('/tmp/parts')
     let contents = []
     await Promise.all(fileNames.map(async (fileName) => {
@@ -19,9 +18,14 @@ describe('<MyComponent /', () => {
     }))
     console.log(fileNames);
     console.log(contents);
-    const contentsWithCustomData = contents.filter(content => content === customData);
+    console.log(customData)
+    const contentsWithCustomData = contents.filter(content => {
+      console.log(JSON.parse(content))
+      console.log(JSON.parse(content) == customData)
+      return JSON.parse(content).deepEqual(customData)
+    });
     console.log(contentsWithCustomData);
-    const customer_details = await fsPromises.readFile('/tmp/customer-details.json', 'utf8')
-    return expect(customer_details).toEqual(JSON.stringify(customData))
+
+    return expect(contentsWithCustomData.length).toEqual(1)
   });
 });
